b60b5d9a563854b048f048052d38a086e3ba501a
HAMOOD\madda <maddasi04@gmail.com>
2023-12-25 01:19:28 -0500

    Added a feature where after the download is completed, the user can choose where to save the video file using filedialog.

--- downloader.py
+++ downloader.py
@@ -1,8 +1,12 @@
 from pytube import YouTube
-import tkinter
-import customtkinter
-import threading
-import os
+import tkinter, customtkinter, threading
+from tkinter import filedialog
+
+def save_file():
+    # Ask user the directory
+    folder = filedialog.askdirectory()
+
+    return folder
 
 def download_video():
     try:
@@ -19,15 +23,12 @@ def download_video():
 
 def download_video_thread(videoLink):
     try:
+        # Creates YouTube Object and how we want to download it
         yt_Object = YouTube(videoLink)
         yd = yt_Object.streams.get_highest_resolution()
         title.configure(text = yt_Object.title, text_color = "black")
-       
-        # Download video into specific directory (feel free to change it for your personal case)
-        os.path.join("C:\\Users\\madda\\Documents\\Videos", f"{yt_Object.title}.mp4")
-        
         downloadInfo.configure(text = "Download Complete!", text_color = "green")
-        yd.download(output_path = "C:\\Users\\madda\\Documents\\Videos")
+        yd.download(output_path = save_file())
     except Exception as err:
         # Update the downloadInfo label from the main thread
         downloadInfo.configure(text=f"An error has occurred: {err}", text_color = "red")
@@ -35,35 +36,35 @@ def download_video_thread(videoLink):
         # Re-enable the download button after the download is complete or an error occurs
         app.after(0, lambda: download.configure(state=tkinter.NORMAL))
 
+if __name__ == "__main__":
+    # System Settings
+    customtkinter.set_appearance_mode("System")
+    customtkinter.set_default_color_theme("blue")
 
-# System Settings
-customtkinter.set_appearance_mode("System")
-customtkinter.set_default_color_theme("blue")
+    # App frame
+    app = customtkinter.CTk()
+    app.geometry("720x480")
+    app.title("YouTube Downloader")
 
-# App frame
-app = customtkinter.CTk()
-app.geometry("720x480")
-app.title("YouTube Downloader")
+    # UI Elements
+    title = customtkinter.CTkLabel(app, text = "Insert a YouTube link")
+    title.pack(padx = 10, pady = 10)
 
-# UI Elements
-title = customtkinter.CTkLabel(app, text = "Insert a YouTube link")
-title.pack(padx = 10, pady = 10)
+    # Link Input
+    url = tkinter.StringVar()
+    link = customtkinter.CTkEntry(app, width = 350, height = 40, textvariable = url)
+    link.pack()
 
-# Link Input
-url = tkinter.StringVar()
-link = customtkinter.CTkEntry(app, width = 350, height = 40, textvariable = url)
-link.pack()
+    # Download Information
+    downloadInfo = customtkinter.CTkLabel(app, text = "")
+    downloadInfo.pack()
 
-# Download Information
-downloadInfo = customtkinter.CTkLabel(app, text = "")
-downloadInfo.pack()
+    # Download Button
+    download = customtkinter.CTkButton(app, text = "Download", command = download_video)
+    download.pack(padx = 10, pady = 10)
 
-# Download Button
-download = customtkinter.CTkButton(app, text = "Download", command = download_video)
-download.pack(padx = 10, pady = 10)
-
-# Run app
-app.mainloop()
+    # Run app
+    app.mainloop()
 
 
 
